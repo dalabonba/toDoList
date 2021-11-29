8ab6a3dcf998ee9ce528a4b3f563fa05bca95e36
dalabonba <40941157@gm.nfu.edu.tw>
2021-11-29 18:23:40 +0800

    any改成get

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/'  , function () {
     return view('welcome');
 });
 
-Route::any('/test',[\App\Http\Controllers\testController::class,'test']);
+Route::get('/test',[\App\Http\Controllers\testController::class,'test']);
+
