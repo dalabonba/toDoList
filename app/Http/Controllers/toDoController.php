b71ee6025b24aacdeb89b4e04b6afb0afff11790
dalabonba <upup588@gmail.com>
2021-12-06 18:34:43 +0800

    新增todocontroller

---
+++ app/Http/Controllers/toDoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class toDoController extends Controller
+{
+    public function index()
+    {
+        $data=DB::table('todos')->get();
+            //dd($data);
+        return view('pages.index',compact('data',$data));
+    }
+    public function create()
+    {
+        return view('pages.create');
+    }
+    public function store(Request $request)
+    {
+        dd($request);
+    }
+}
