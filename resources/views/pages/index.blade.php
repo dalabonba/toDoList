b4318322970535d7fb15151074eb3b363b7205e3
dalabonba <upup588@gmail.com>
2021-12-13 18:15:34 +0800

    新增按鈕

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-    <table>
+    <table class="table">
         <thead>
             <tr>
                 <td>標題</td>
                 <td>內容</td>
                 <td>備註</td>
+                <td>功能</td>
             </tr>
         </thead>
         <tbody>
@@ -15,6 +16,9 @@
                 <td>{{$row->title}}</td>
                 <td>{{$row->content}}</td>
                 <td>{{$row->remark}}</td>
+                <td><button type="button" class="btn btn-outline-success">新增</button>
+                    <button type="button" class="btn btn-outline-danger" onclick="">刪除</button>
+                </td>
             </tr>
         @endforeach
         </tbody>
