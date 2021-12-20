afaa1e6b2b10a592ffec4b14266eaa84d484a0e3
dalabonba <upup588@gmail.com>
2021-12-21 03:21:39 +0800

    新增了編輯的controller function

--- app/Http/Controllers/toDoController.php
+++ app/Http/Controllers/toDoController.php
@@ -13,15 +13,16 @@ class toDoController extends Controller
             //dd($data);
         return view('pages.index',compact('data',$data));
     }
+
     public function getCreatePage()
     {
         return view('pages.create');
     }
     public function storeCreateData(Request $request)
     {
-        $title = $request->title;
-        $content=$request->content;
-        $remark=$request->remark;
+        $title = $request->get('title');
+        $content = $request->get('content');
+        $remark = $request->get('remark');
 
         DB::table('todos')->insert([
            'title'=>$title,
@@ -31,6 +32,7 @@ class toDoController extends Controller
 
         return view('pages.create');
     }
+
     public function deleteData(Request $request)
     {
         $id=$request->get("id");
@@ -38,5 +40,30 @@ class toDoController extends Controller
             ->where('id',$id)
             ->delete();
         return redirect()->route("index");
+    }
+
+    public function getEditPage(Request $request)
+    {
+        $id = $request->get('id');
+        $data = DB::table('todos')
+            ->where('id', $id)
+            ->first();
+        return view('pages.update',compact('data',$data));
+    }
+    public function storeEditData(Request $request)
+    {
+        $id = $request->get('id');
+        $title = $request->get('title');
+        $content = $request->get('content');
+        $remark = $request->get('remark');
+
+        DB::table('todos')
+            ->where('id', $id)
+            ->update([
+                'title' => $title,
+                'content' => $content,
+                'remark' => $remark,
+            ]);
+        return redirect()->route('index');
     }
 }
